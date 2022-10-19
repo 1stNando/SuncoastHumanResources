79239a5ec9ea48cc90e02d75eaead9ba7d99cf78
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-10-19 16:57:03 -0400

    Improves our searching code by using LINQ

--- Program.cs
+++ Program.cs
@@ -68,13 +68,54 @@ namespace SuncoastHumanResources
             {
                 //Insert a blank line then promt them and get their answer (force uppercase)
                 Console.WriteLine();
-                Console.Write("What do you want to do? (A)dd an employee or (Q)uit:");
+                Console.Write("What do you want to do?\n(A)dd an employee\n(F)ind an employee\n(S)how all the employees\n(Q)uit\n:");
                 var choice = Console.ReadLine().ToUpper();
 
                 if (choice == "Q")
                 {
                     //They said quit, so set our keepGoing to false
                     keepGoing = false;
+                }
+                else
+                if (choice == "F")
+                {
+                    Console.WriteLine("FINDING");
+                    // - Create a varialbe named **'foundEmployee'**  null value
+                    Employee foundEmployee = null;
+
+                    // Prompt for the name
+                    var nameToSearchFor = PromptForString("What name are you looking for?");
+                    //Show the use of LINQ method shortcut style to search for something.!SUPERPOWER.!!!!!!
+                    Employee foundEmployee = employees.FirstOrDefault(employee => employee.Name == nameToSearchFor);
+
+                    /*/Loop through the list to look for a match
+                    foreach (var employee in employees)
+                    {
+                        //If we find one, update 'foundEmployee'
+                        if (employee.Name == nameToSearchFor)
+                        {
+                            foundEmployee = employee;
+                        }
+                    }*///the above is no longer need this look when we utilize LINQ method FirstOrDefault().
+
+                    //After the loop, 'foundEmployee' is either 'null' (not found) or refers to the matching item
+                    if (foundEmployee == null)
+                    {
+                        Console.WriteLine("No such person!");
+                    }
+                    //Show a message if 'null', otherwise show the details.
+
+
+                }
+
+                else//the following adds Show employee 
+                if (choice == "S")
+                {
+                    // READ(out of CREATE - READ - UPDATE - DELETE)
+                    foreach (var employee in employees)
+                    {
+                        Console.WriteLine($"{employee.Name} is in department {employee.Department} and makes ${employee.Salary}");
+                    }
                 }
                 else
                 {
@@ -88,7 +129,7 @@ namespace SuncoastHumanResources
 
                     Console.WriteLine($"Hello, {employee.Name} you make {employee.MonthlySalary()} dollars per month.");
 
-                    //Keep employee data around by:
+                    // Add it to the list
                     employees.Add(employee);
                 }
                 // end of the 'while' statement
